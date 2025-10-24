2decde0ab90a74298b416a12ce2faf7d634549c4
SeaotterMS <peter910820@gmail.com>
2025-10-24 18:13:43 +0800

    chore: add ALLOW_CORS

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 	"github.com/joho/godotenv"
 	"github.com/sirupsen/logrus"
 
@@ -43,6 +44,12 @@ func main() {
 	initTokenCache()
 
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: os.Getenv("ALLOW_CORS"),
+		AllowMethods: "GET,POST,PUT,DELETE,OPTIONS",
+		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
+	}))
 
 	// api route group
 	apiGroup := app.Group("/api") // main api route group
